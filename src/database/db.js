79767ec551fae673b490c0dc480a964f90d2999b
rflnz <rafaelanunesacosta@gmail.com>
2025-05-05 15:25:54 -0400

    add
    atualizacao

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql2');
 
-// Conexão com seu banco MySQL
+
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',          // se tiver senha no seu XAMPP, coloque aqui
-  database: 'soberano'   // nome do seu banco
+  password: '',          
+  database: 'soberano'  
 });
 
-// Conecta e verifica
 db.connect((err) => {
   if (err) {
     console.error('Erro ao conectar ao MySQL:', err.message);
